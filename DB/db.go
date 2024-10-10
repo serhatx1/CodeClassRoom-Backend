80ed1605f82af0f68e1e3528e92fb366f9a3e192
Serhat Arslan <49308319+serhatx1@users.noreply.github.com>
2024-10-10 18:51:01 +0300

    db

--- DB/db.go
+++ DB/db.go
@@ -1,6 +1,8 @@
 package DB
 
 import (
+	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,8 +11,17 @@ func DB() *gorm.DB {
 	dsn := "root:root@tcp(127.0.0.1:3306)/eCode?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
-		return db
+		log.Fatalf("failed to connect database: %v", err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get database instance: %v", err)
+	}
+
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(0)
+
 	return db
 }
