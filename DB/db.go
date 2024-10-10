cc819d244165698a5c758b6b7b6a68446b684f31
Serhat Arslan <49308319+serhatx1@users.noreply.github.com>
2024-10-10 19:44:25 +0300

    up

--- DB/db.go
+++ DB/db.go
@@ -2,26 +2,35 @@ package DB
 
 import (
 	"log"
+	"sync"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var dbInstance *gorm.DB
+var once sync.Once
+
 func DB() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:3306)/eCode?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("failed to connect database: %v", err)
-	}
+	once.Do(func() {
+		dsn := "root:root@tcp(127.0.0.1:3306)/eCode?charset=utf8mb4&parseTime=True&loc=Local"
+		var err error
+		dbInstance, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatalf("failed to connect database: %v", err)
+		}
 
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatalf("failed to get database instance: %v", err)
-	}
+		sqlDB, err := dbInstance.DB()
+		if err != nil {
+			log.Fatalf("failed to get database instance: %v", err)
+		}
 
-	sqlDB.SetMaxOpenConns(10)
-	sqlDB.SetMaxIdleConns(5)
-	sqlDB.SetConnMaxLifetime(0)
+		// Connection pool settings
+		sqlDB.SetMaxOpenConns(25)
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetConnMaxLifetime(60 * time.Minute)
+	})
 
-	return db
+	return dbInstance
 }
